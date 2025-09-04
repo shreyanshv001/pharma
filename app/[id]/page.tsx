017bc952bdb235129adb9dabe270efc911336afb
shreyanshv001 <222shreyanshverma@gmail.com>
2025-09-04 12:20:42 +0530

    tried to fix 2

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,10 +1,7 @@
+"use client";
+
 import InstrumentDetailClient from "./InstrumentDetailClient";
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
-  // Pass the id down to the client component
+export default function Page({ params }: { params: { id: string } }) {
   return <InstrumentDetailClient instrumentId={params.id} />;
 }

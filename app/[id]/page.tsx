d4148a0f05577af3919a4150b18a0cfdb1cecb3d
shreyanshv001 <222shreyanshverma@gmail.com>
2025-09-04 13:00:08 +0530

    same error 1

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,13 @@
 import InstrumentDetailClient from "./InstrumentDetailClient";
 
-// In app/experiment/[id]/page.tsx
-export default async function Page(props: PageProps<'/experiment/[id]'>) {
-  const { id } = await props.params;
-  return <InstrumentDetailClient instrumentId={id} />;
+
+interface PageProps {
+  params: { id: string };
+}
+
+export default async function Page({ params }: PageProps) {
+  const instrumentId = params.id;
+
+  // optionally fetch data here if needed
+  return <InstrumentDetailClient instrumentId={instrumentId} />;
 }
